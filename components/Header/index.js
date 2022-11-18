901436e68e937ffda04aa19f2b01e281d6f33a30
rohithrajbaggam <rohithbaggam.github@gmail.com>
2022-11-18 10:31:21 +0530

    final push

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = (props) => {
   return (
     <div className="glass1 flex items-center p-5 text-white justify-between">
-      <h1 className="font-bold text-[20px]">Data Science And Analysis</h1>
+      <h1 className="font-bold text-[20px]">CRYPTOCURRENCY DATA ANALYSIS</h1>
       <div className="flex items-center space-x-5">
         <Link href={"/"}>
           <p>Visualizer</p>
